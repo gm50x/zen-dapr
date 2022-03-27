1016eb5663314a68b35fd987d7bd1b8f5e975755
Getúlio Magela Silva <getuliomagela@outlook.com>
2022-03-27 18:25:04 -0300

    get Prisma url from dapr get secrets

--- libs/prisma/src/prisma.module.ts
+++ libs/prisma/src/prisma.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DaprModule } from '@zen/dapr';
+
 import { PrismaService } from './services';
+import { PrismaServiceProvider } from './providers';
 
 @Module({
-  providers: [PrismaService],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DaprModule],
+  providers: [PrismaServiceProvider],
   exports: [PrismaService],
 })
 export class PrismaModule {}
