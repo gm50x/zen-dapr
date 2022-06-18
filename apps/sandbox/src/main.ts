93602ad9a4f7b0cf9f11f4204ebe7845ac478c31
Getúlio Magela Silva <getuliomagela@outlook.com>
2022-06-18 12:51:32 -0300

    adds unified configurator

--- apps/sandbox/src/main.ts
+++ apps/sandbox/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { PrismaService } from '@zen/prisma';
+import { Configurator } from 'libs/config/src';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  new Configurator(app).unified({
+    swagger: {
+      title: 'Sandbox',
+      description:
+        'Demonstrates monorepo powered by NestJS, Dapr, Prisma and Docker for Autonomous Services(Micro too)',
+    },
+  });
 
   const prisma = app.get(PrismaService);
   prisma.enableShutdownHooks(app);
